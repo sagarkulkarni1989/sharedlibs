9dfefb38678c2c3f20f9f457e92ea3cbf0e73104
Sagar Kulkarni <46215433+sagarkulkarni1989@users.noreply.github.com>
2024-04-07 05:32:22 +0530

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,13 +1,8 @@
 def call(String repoUrl, String branch = 'main') {
-    pipeline {
-        agent any
-        stages {
-            stage('Git Checkout') {
-                steps {
-                    script {
-                        git branch: branch, url: repoUrl
-                    }
-                }
+    stage('Git Checkout') {
+        steps {
+            script {
+                git branch: branch, url: repoUrl
             }
         }
     }
